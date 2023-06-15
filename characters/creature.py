5694c46b789130a464dc399a458a3b3caceb9023
Ckmpls216 <101603041+Ckmpls216@users.noreply.github.com>
2023-06-15 06:41:35 -0500

    Create creature.py

---
+++ characters/creature.py
@@ -0,0 +1,13 @@
+import random
+
+
+# Creature class
+class Creature:
+    def __init__(self, health, attack, defense):
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+
+    def attack(self, target):
+        # Implement the logic for the creature attacking a target here
+        pass
